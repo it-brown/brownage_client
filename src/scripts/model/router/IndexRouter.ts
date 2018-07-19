2437b018970292d12b6cbbe7a7b378face3f251d
brown <joanh4mjst@gmail.com>
2018-07-19 23:51:37 +0900

    change router option mode

--- src/scripts/model/router/IndexRouter.ts
+++ src/scripts/model/router/IndexRouter.ts
@@ -1,16 +1,14 @@
 import VueRouter from 'vue-router';
+import Home from '@/components/home/Home.vue';
 import Gantt from '@/components/chart/Gantt.vue';
-
-const User = {
-    template: '<div>User {{ $route.params.id }}</div>'
-}
 
 export default class IndexRouter extends VueRouter {
     public constructor() {
         super({
-            mode: 'history',
+            mode: 'hash',
             routes: [
-                { path: '/project/:id', component: User }
+                { path: '/', component: Home },
+                { path: '/project/:name', component: Gantt }
             ]
         })
     }
