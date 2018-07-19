0d142eeab965872e2b7603edec9131dd11bbe4f9
brown <joanh4mjst@gmail.com>
2018-07-19 16:35:48 +0900

    created component to add tasks. needs modal

--- src/components/common/TaskInput.vue
+++ src/components/common/TaskInput.vue
@@ -1,7 +1,12 @@
 <template lang='pug'>
 .vue-task-input
-    .columns.is-gapless
+    .task-header.columns.is-gapless.is-multiline
         .column.cell-height.border-grid(v-for='cell in inputCells' :key='cell.id' :class='cell.cellStyle'): span {{ cell.title }}
+        .task-add(@click='addTask'): b-icon(icon='plus')
+
+    .task-row.columns.is-gapless(v-for='i in 3' :key='i')
+        .column.cell-height.is-clipped(v-for='cell in inputCells' :key='cell.id' :class='cell.cellStyle'): span.padding-light test
+        .task-add(@click='addTask'): b-icon(icon='plus')
 </template>
 
 <script lang='ts'>
@@ -13,10 +18,14 @@ import { Vue, Component } from 'vue-property-decorator';
 @Component
 export default class TaskInput extends Vue {
     protected inputCells = [
-        { id: 1, title: 'Task name', cellStyle: 'is-6' },
+        { id: 1, title: 'Task name', cellStyle: 'is-5' },
         { id: 2, title: 'Start time', cellStyle: 'is-3' },
-        { id: 3, title: 'Duration', cellStyle: 'is-3' }
+        { id: 3, title: 'Duration', cellStyle: 'is-4' }
     ]
+
+    protected addTask(): void {
+        this.$dialog.alert('hoge');
+    }
 }
 </script>
 
@@ -24,4 +33,11 @@ export default class TaskInput extends Vue {
 @import 'entry/variable'
 
 .vue-task-input
+    .task-row, .task-header
+        position: relative
+
+        .task-add
+            position: absolute
+            right: 0
+            color: $primary
 </style>
