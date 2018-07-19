0ffab904e2f49c1aaca5b1bfc8386d22c8db3c30
brown <joanh4mjst@gmail.com>
2018-07-19 23:54:22 +0900

    miner rename

--- src/scripts/model/chart/ProjectSchedule.ts
+++ src/scripts/model/chart/ProjectSchedule.ts
@@ -2,6 +2,6 @@ import { TaskItem } from '@/scripts/model/chart/TaskItem';
 
 export interface ProjectSchedule {
     id: number;
-    projectName: string;
-    tasks: TaskItem[]
+    name: string;
+    tasks: TaskItem[];
 }
