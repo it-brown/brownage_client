1d345676d504ca045f9bdc2c78a05543fb725699
brown <joanh4mjst@gmail.com>
2018-07-23 13:48:49 +0900

    add 404 page

---
+++ src/components/common/PageNotFound.vue
@@ -0,0 +1,24 @@
+<template lang='pug'>
+.vue-page-not-found
+    section.section
+        .content.has-text-grey.has-text-centered
+            p: b-icon(icon='emoticon-sad' size='is-large')
+            p Page not found.
+</template>
+
+<script lang='ts'>
+import { Vue, Component } from 'vue-property-decorator';
+
+/**
+ * Vue Component
+ */
+@Component
+export default class PageNotFound extends Vue {
+}
+</script>
+
+<style lang='sass' scoped>
+@import 'entry/variable'
+
+.vue-page-not-found
+</style>
