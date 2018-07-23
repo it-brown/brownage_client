7a9997fef0a70929a2bcc4c6a799c8216f979885
brown <joanh4mjst@gmail.com>
2018-07-23 16:14:48 +0900

    give props to modal for control

--- src/components/chart/TaskInput.vue
+++ src/components/chart/TaskInput.vue
@@ -1,13 +1,14 @@
 <template lang='pug'>
 .vue-task-input
-    task-edit-modal(:isActive.sync='isComponentModalActive')
+    task-edit-modal(:isActive.sync='isComponentModalActive' :mode='taskModalMode' :taskId='taskId')
     .task-header.columns.is-gapless
-        .column.auto.task-add(@click='isComponentModalActive = true'): b-icon(icon='plus')
+        .column.auto.add-task(@click='addTask'): b-icon(icon='plus')
         .column.cell-height.border-grid(v-for='cell in inputCells' :key='cell.id' :class='cell.cellStyle'): span {{ cell.title }}
 
     .task-row.columns.is-gapless(v-for='task in tasks' :key='task.id')
-        .column.auto.task-add(@click='isComponentModalActive = true'): b-icon(icon='plus')
-        .column.cell-height.is-clipped(v-for='cell in inputCells' :key='cell.id' :class='cell.cellStyle'): span.padding-light {{ task[cell.key] }}
+        //- TODO: add task nesting action. link tasks.
+        //-.column.auto.add-task(@click='addTask'): b-icon(icon='plus')
+        .column.cell-height.is-clipped(v-for='cell in inputCells' :key='cell.id' :class='cell.cellStyle' @dblclick='updateTask(task.id)'): span.padding-light {{ task[cell.key] }}
 </template>
 
 <script lang='ts'>
@@ -28,6 +29,8 @@ export default class TaskInput extends Vue {
     @Prop({ type: Array })
     protected tasks?: TaskItem[];
 
+    protected taskModalMode: 'create' | 'update' = 'create';
+    protected taskId: number | null = null;
     protected isComponentModalActive = false;
 
     protected inputCells = [
@@ -35,6 +38,17 @@ export default class TaskInput extends Vue {
         { id: 2, title: 'Start time', cellStyle: 'is-3', key: 'start_date' },
         { id: 3, title: 'Duration', cellStyle: 'is-4', key: 'duration' }
     ]
+
+    protected addTask(): void {
+        this.isComponentModalActive = true;
+        this.taskModalMode = 'create';
+    }
+
+    protected updateTask(taskId: number): void {
+        this.isComponentModalActive = true;
+        this.taskModalMode = 'update';
+        this.taskId = taskId;
+    }
 }
 </script>
 
@@ -45,7 +59,7 @@ export default class TaskInput extends Vue {
     .task-row, .task-header
         position: relative
 
-        .task-add
+        .add-task
             position: absolute
             right: 0
             color: $primary
