5598cccba227ea10f423e87f2a1e4af55ff2cbef
brown <joanh4mjst@gmail.com>
2018-07-23 14:11:05 +0900

    move dir

--- src/components/chart/TaskInput.vue
+++ src/components/chart/TaskInput.vue
@@ -1,11 +1,12 @@
 <template lang='pug'>
 .vue-task-input
+    task-edit-modal(:isActive.sync='isComponentModalActive')
     .task-header.columns.is-gapless
-        .column.auto.task-add(@click='addTask'): b-icon(icon='plus')
+        .column.auto.task-add(@click='isComponentModalActive = true'): b-icon(icon='plus')
         .column.cell-height.border-grid(v-for='cell in inputCells' :key='cell.id' :class='cell.cellStyle'): span {{ cell.title }}
 
     .task-row.columns.is-gapless(v-for='task in tasks' :key='task.id')
-        .column.auto.task-add(@click='addTask'): b-icon(icon='plus')
+        .column.auto.task-add(@click='isComponentModalActive = true'): b-icon(icon='plus')
         .column.cell-height.is-clipped(v-for='cell in inputCells' :key='cell.id' :class='cell.cellStyle'): span.padding-light {{ task[cell.key] }}
 </template>
 
@@ -13,24 +14,27 @@
 import { Vue, Component, Prop } from 'vue-property-decorator';
 
 import { TaskItem } from '@/scripts/model/chart/TaskItem';
+import TaskEditModal from '@/components/chart/TaskEditModal.vue';
 
 /**
  * Vue Component
  */
-@Component
+@Component({
+    components: {
+        TaskEditModal
+    }
+})
 export default class TaskInput extends Vue {
     @Prop({ type: Array })
     protected tasks?: TaskItem[];
+
+    protected isComponentModalActive = false;
 
     protected inputCells = [
         { id: 1, title: 'Task name', cellStyle: 'is-5', key: 'text'},
         { id: 2, title: 'Start time', cellStyle: 'is-3', key: 'start_date' },
         { id: 3, title: 'Duration', cellStyle: 'is-4', key: 'duration' }
     ]
-
-    protected addTask(): void {
-        this.$dialog.alert('hoge');
-    }
 }
 </script>
 
